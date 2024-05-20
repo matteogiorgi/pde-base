db0fdf7a188002dd2bf39cf5fef7f7041be928e4
Matteo <mattegiorgi@gmail.com>
2024-05-20 18:58:32 +0200

    add index

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@
 RED='\033[1;36m'
 NC='\033[0m'
 # ---
-if [[ -d "${HOME}/.pde-restore" ]]; then
+if [[ -d "${HOME}/.confrestore" ]]; then
     printf "\n${RED}%s${NC}"   "════════ Warning: pde-conf already set ════════"
-    printf "\n${RED}%s${NC}\n" "Remove ~/.pde-restore and run this script again"
+    printf "\n${RED}%s${NC}\n" "Remove ~/.confrestore and run this script again"
     exit 1
 fi
 # ---
@@ -47,7 +47,7 @@ function store-conf () {
             fi
         fi
     }
-    RESTORE="${HOME}/.pde-restore" && mkdir "${RESTORE}"
+    RESTORE="${HOME}/.confrestore" && command mkdir -p "${RESTORE}"
     backup-conf "${HOME}/.bash_logout"
     backup-conf "${HOME}/.bashrc"
     backup-conf "${HOME}/.profile"
