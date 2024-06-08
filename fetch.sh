ef571c0341c909a9d6453ae8ece6b4635ea641e0
Matteo <mattegiorgi@gmail.com>
2024-06-08 16:19:03 +0200

    update

--- fetch.sh
+++ fetch.sh
@@ -33,8 +33,8 @@ C6='[1;36m'
 ### Output
 ##########
 
-printf '%s\n' "          ${C4}${MYNAME}${C0}${CB}@${C4}${MYHOST}"
-printf '%s\n' "${C5}  (\_/)   ${C6}OS${C0}      ${MYOSYS}"
+printf '%s\n' "${C5}  .   .   ${C4}${MYNAME}${C0}${CB}@${C4}${MYHOST}"
+printf '%s\n' "${C5}  |\_/|   ${C6}OS${C0}      ${MYOSYS}"
 printf '%s\n' "${C5}  (${C0}${CB}O${C5}.${C0}${CB}o${C5})   ${C6}Kernel${C0}  ${MYKERNEL}"
 printf '%s\n' "${C5}  (> <)   ${C6}Shell${C0}   ${MYSHELL}"
 printf "\n"
